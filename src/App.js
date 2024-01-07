8f4c256f22f61da86602d45b03c18d0fc848021e
oayman0 <omaraymankhalaf@gmail.com>
2024-01-07 17:43:40 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import UserInterface from './components/UserInterface';
+
+
+function App() {
+  return (
+    <div className="App">
+      <UserInterface/>
+    </div>
+  );
+}
+
+export default App;
